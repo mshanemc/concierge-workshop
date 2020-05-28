671467c6cc4cb88897aad20e6ad9a74d7e7efb67
mshanemc <shane.mclaughlin@salesforce.com>
2020-05-28 15:55:59 -0500

    concierge requirements

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,5 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+# sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 30 -s -n --userprefix electron --userdomain ee.demo
+
 sfdx force:source:push
 sfdx force:org:open
