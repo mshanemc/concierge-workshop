b823054dfd3ffe285244fa91997721d15f001344
mshanemc <shane.mclaughlin@salesforce.com>
2020-05-28 15:59:34 -0500

    copy concierge stuff from solutions org

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
 # sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 30 -s -n --userprefix electron --userdomain ee.demo
+sfdx force:data:record:update -s User -w "Name='User User'" -v "UserPermissionsKnowledgeUser=true"
 
 sfdx force:source:push
 sfdx force:org:open
