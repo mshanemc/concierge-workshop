02efac2fdcc3dda662f15037a319bbe3da41cfae
mshanemc <shane.mclaughlin@salesforce.com>
2020-05-28 16:05:10 -0500

    knowledge perms and setup

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,11 @@
 # sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 30 -s -n --userprefix electron --userdomain ee.demo
+sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 30 -s -n --userprefix concierge --userdomain work.shop
 sfdx force:data:record:update -s User -w "Name='User User'" -v "UserPermissionsKnowledgeUser=true"
 
 sfdx force:source:push
+sfdx force:user:permset:assign -n solutions
+sfdx force:data:bulk:upsert -f data/Knowledge__kav.csv -i id -s Knowledge__kav --wait 30
+
+sfdx force:apex:execute -f scripts/conciergeSetup.cls
+
 sfdx force:org:open
