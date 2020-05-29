648d453de081aeffeb51ac99f5c4d6bfe65f0d89
mshanemc <shane.mclaughlin@salesforce.com>
2020-05-29 14:28:05 -0500

    one day

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 # sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 30 -s -n --userprefix concierge --userdomain work.shop
+sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 1 -s -n --userprefix concierge --userdomain work.shop
 sfdx force:data:record:update -s User -w "Name='User User'" -v "UserPermissionsKnowledgeUser=true"
 
 sfdx force:source:push
