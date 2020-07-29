e304a339957e55106d0ab8786940ee30179122bf
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-29 09:25:36 -0500

    new package

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
-# sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 1 -s -n --userprefix concierge --userdomain work.shop
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -n --userprefix concierge --userdomain work.shop
+sfdx force:package:install --package 04t4P000002qm6f --noprompt --wait 50
 sfdx force:data:record:update -s User -w "Name='User User'" -v "UserPermissionsKnowledgeUser=true"
 
 sfdx force:source:push
